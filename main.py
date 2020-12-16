052c46b646b03c88b248a1ac7d4403d1c3372e5e
memareve <yqqN87cjbeER>
2020-12-16 13:23:04 +0700

    beta

--- main.py
+++ main.py
@@ -1,29 +1,80 @@
 # Case-study #3
-# Developers: Marinkin O. (%),
+# Developers: Marinkin O. (85%),
 # Seledtsov A. (%),
 # Evdischenko M. (%)
 
+from decimal import Decimal
 
-s = int(input('начальные вложения '))
-dop = int(input('Инвестиционные вливания '))
-period = str(input('Месяц,год, день? '))
-period.lower()
-percent = input('процентная ставка ')
-percent.replace(',', '.')
-percent = float(percent)
+print('Вы хотите открыть вклад на год или более? (да/нет)')
+answer = input().lower().replace(' ', '')
+if answer == 'да':
+    n = int(input('Срок размещения капитала (лет):'))
+else:
+    n = int(input('Срок размещения капитала (мес.):'))
+    if n == 12:
+        answer = 'да'
+        n = 1
+d = input('Период, по итогам которого происходит начисление процентов (месяц/год):').lower().replace(' ', '')
+s = float(input('Начальный капитал ($):'))
+percent = input('Процентная ставка (%/мес.):').replace(',', '.')
 
+percent = float(int(percent) / 100)
+dop = int(input('Инвестиционные вливания ($/мес.):'))
+capital = 0
 
-n = int(input('количество периодов '))
-a = 1
-while a < n:
-    if a == 1:
-        sum = s * (1+ percent )
-        print('меясц', a)
-        print('капитал', sum)
-    s = sum + dop
-    print('месяц ', a)
-    print('основа инвестиции', round(s, 2))
-    sum = s * (1+ percent )
-    print('month sum', round(sum - s, 2 ))
-    print('capital', round(sum, 2))
-    a += 1
+if d == 'месяц':
+    if answer == 'да':
+        for i in range(1,n+1):
+            print(i, 'год')
+            print('''-----------------------------------------------------
+|          |    основа    |  сумма %    |            |
+|  месяц   |  инвестиций  |  за месяц   |  капитал   |
+------------------------------------------------------''')
+            for i in range(1,13):
+                if i >= 10:
+                    print('|    ', i, '    |', Decimal(s).quantize(Decimal("1.00")), '          |', Decimal(s * percent).quantize(Decimal("1.00")), '  |', Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
+                    capital = s * percent + s
+                    s = capital + dop
+                else:
+                    print('|    ', i, '     |', Decimal(s).quantize(Decimal("1.00")), '          |', Decimal(s * percent).quantize(Decimal("1.00")), '  |', Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
+                    capital = s * percent + s
+                    s = capital + dop
+            print('''------------------------------------------------------
+         ''')
+    else:
+        print(1, 'год')
+        print('''-----------------------------------------------------
+    |          |    основа    |  сумма %    |            |
+    |  месяц   |  инвестиций  |  за месяц   |  капитал   |
+    ------------------------------------------------------''')
+        for i in range(1,n+1):
+            if i >= 10:
+                print('|    ', i, '    |', Decimal(s).quantize(Decimal("1.00")), '  |', Decimal(s * percent).quantize(Decimal("1.00")), '  |', Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
+                capital = s * percent + s
+                s = capital + dop
+            else:
+                print('|    ', i, '     |', Decimal(s).quantize(Decimal("1.00")), '  |', Decimal(s * percent).quantize(Decimal("1.00")), '  |', Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
+                capital = s * percent + s
+                s = capital + dop
+            print('''------------------------------------------------------
+         ''')
+else:
+    print('''-----------------------------------------------------
+    |         |    основа    |  сумма %    |            |
+    |   год   |  инвестиций  |  за год     |  капитал   |
+    ------------------------------------------------------''')
+    for i in range(1, n + 1):
+        if i >= 10:
+            print('|    ', i, '    |', Decimal(s).quantize(Decimal("1.00")), '  |',
+                  Decimal(s * percent).quantize(Decimal("1.00")), '  |',
+                  Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
+            capital = s * percent + s
+            s = capital + dop
+        else:
+            print('|    ', i, '    |', Decimal(s).quantize(Decimal("1.00")), '  |',
+                  Decimal(s * percent).quantize(Decimal("1.00")), '  |',
+                  Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
+            capital = s * percent + s
+            s = capital + dop
+        print('''------------------------------------------------------
+     ''')
