b84515e340238aba3a24a7fb0acab67cc3de9b00
memareve <yqqN87cjbeER>
2020-12-17 07:43:55 +0700

    final

--- main.py
+++ main.py
@@ -5,17 +5,23 @@
 
 from decimal import Decimal
 from prettytable import PrettyTable
+
 tb = PrettyTable()
 
-
-
-print('Вы хотите открыть вклад на год или более? (да/нет)')
-answer = input().lower().replace(' ', '')
+quest = input('Вы хотите открыть вклад? (да/нет):').lower().replace(' ', '')
+if quest == 'да':
+    print('Отлично!')
+else:
+    print('Очень грустно...')
+    exit()
+answer = input('Вы хотите открыть вклад на год или более? (да/нет):').lower().replace(' ', '')
+d = input('Период, по итогам которого происходит начисление процентов (месяц/год):').lower().replace(' ', '')
 if answer == 'да':
     n = int(input('Срок размещения капитала (лет):'))
-    d = input('Период, по итогам которого происходит начисление процентов (месяц/год):').lower().replace(' ', '')
 else:
     n = int(input('Срок размещения капитала (мес.):'))
+    if d == 'год' and n < 12:
+        print('Мы можем предложить вам только начисление по месяцам, т.к. срок вашего вклада меньше года.')
     if n == 12:
         answer = 'да'
         n = 1
@@ -25,53 +31,32 @@ percent = float(int(percent) / 100)
 dop = int(input('Инвестиционные вливания ($/мес.):'))
 capital = 0
 
-
-tb.field_names = ['месяц', 'основа инвестиций', 'сумма % за месяц', 'капитал']
-tb.add_row([1, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")), Decimal(s * percent + s).quantize(Decimal("1.00"))])
-
 if d == 'месяц':
     if answer == 'да':
-        for i in range(1,n+1):
+        for i in range(1, n + 1):
             print(i, 'год')
-            tb.columns.header = ['месяц', 'основа\nинвестиций', 'сумма %\nза месяц', 'капитал']
-            for i in range(1,13):
-                if i >= 10:
-                    print(i, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")), Decimal(s * percent + s).quantize(Decimal("1.00")))
-                    capital = s * percent + s
-                    s = capital + dop
-                else:
-                    print(i, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")), Decimal(s * percent + s).quantize(Decimal("1.00")))
-                    capital = s * percent + s
-                    s = capital + dop
-
+            tb.field_names = ['месяц', 'основа инвестиций', 'сумма % за месяц', 'капитал']
+            for j in range(1, 13):
+                tb.add_row([j, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")),
+                            Decimal(s * percent + s).quantize(Decimal("1.00"))])
+                capital = s * percent + s
+                s = capital + dop
+            print(tb)
+            tb.clear_rows()
     else:
         print(1, 'год')
-        for i in range(1,n+1):
-            tb.add_row([i, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")), Decimal(s * percent + s).quantize(Decimal("1.00"))])
+        tb.field_names = ['месяц', 'основа инвестиций', 'сумма % за месяц', 'капитал']
+        for i in range(1, n + 1):
+            tb.add_row([i, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")),
+                        Decimal(s * percent + s).quantize(Decimal("1.00"))])
             capital = s * percent + s
             s = capital + dop
         print(tb)
-
-
-
-
 else:
-    print('''-----------------------------------------------------
-|         |    основа    |  сумма %    |            |
-|   год   |  инвестиций  |  за год     |  капитал   |
-------------------------------------------------------''')
+    tb.field_names = ['год', 'основа инвестиций', 'сумма % за год', 'капитал']
     for i in range(1, n + 1):
-        if i >= 10:
-            print('|    ', i, '    |', Decimal(s).quantize(Decimal("1.00")), '  |',
-                  Decimal(s * percent).quantize(Decimal("1.00")), '  |',
-                  Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
-            capital = s * percent + s
-            s = capital + dop
-        else:
-            print('|    ', i, '    |', Decimal(s).quantize(Decimal("1.00")), '  |',
-                  Decimal(s * percent).quantize(Decimal("1.00")), '  |',
-                  Decimal(s * percent + s).quantize(Decimal("1.00")), '  |', sep='')
-            capital = s * percent + s
-            s = capital + dop
-    print('''------------------------------------------------------
-     ''')
+        tb.add_row([i, Decimal(s).quantize(Decimal("1.00")), Decimal(s * percent).quantize(Decimal("1.00")),
+                    Decimal(s * percent + s).quantize(Decimal("1.00"))])
+        capital = s * percent + s
+        s = capital + dop
+    print(tb)
